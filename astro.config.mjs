bcfe8ac866d85f23c83b6c8389023941df1ef9a9
maxxus <66540504+MaxxusX@users.noreply.github.com>
2025-07-07 13:50:58 -0500

    BREAKING: Deprecate Support for Old Browsers

--- astro.config.mjs
+++ astro.config.mjs
@@ -57,23 +57,19 @@ export default defineConfig({
 				sourceMap: isDevBuild,
 				errorRecovery: false,
 				targets: {
-					and_chr: 128 << 16,
-					and_ff: 127 << 16,
-					android: 128 << 16,
-					chrome: 109 << 16, // vite default: 87
-					edge: 126 << 16, // vite default: 88
-					firefox: 115 << 16, // vite default: 78
-					ios_saf: 16 << 16,
-					op_mob: 80 << 16,
-					opera: 109 << 16,
-					safari: 16 << 16, // vite default: 14
-					samsung: 25 << 16,
+					chrome: 112 << 16, // vite default: 87
+					and_chr: 138 << 16,
+					edge: 112 << 16, // vite default: 88
+					firefox: 128 << 16, // vite default: 78
+					and_ff: 140 << 16,
+					safari: (16 << 16) | (6 << 8), // vite default: 14
+					ios_saf: (16 << 16) | (7 << 8),
 				},
 			},
 		},
 		build: {
 			// vite default: ["es2020", "edge88", "firefox78", "chrome87", "safari14"]
-			target: ["es2020", "edge126", "firefox115", "chrome109", "safari16"],
+			target: ["es2020", "chrome112", "edge112", "firefox128", "safari16.6"],
 			assetsInlineLimit: 0,
 			cssCodeSplit: false,
 			cssMinify: "lightningcss",
