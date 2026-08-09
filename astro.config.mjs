21df8a2e08be38d766d725581faf1e85d44ebf63
maxxus <66540504+MaxxusX@users.noreply.github.com>
2026-08-09 01:19:51 -0500

    fix(astro.config.mjs): default site to script.nullfire.dev
    Signed-off-by: maxxus <66540504+MaxxusX@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig, passthroughImageService } from "astro/config";
 const isDevBuild = env.NODE_ENV === "development" || env.MODE === "development";
 
 export default defineConfig({
-	site: env.SITE_URL_OVERRIDE ?? "https://nullfire.pages.dev/",
+	site: env.SITE_URL_OVERRIDE ?? "https://script.nullfire.dev/",
 	base: "/",
 	trailingSlash: "ignore",
 	output: "static",
